33a63a2cba31b4220b9ceee1d68f79d72b7453e5
Paulo Salvatore <paulo@salvatore.tech>
2020-09-02 16:29:45 -0300

    Exemplo trabalhando com bibliotecas externas e importando via gradle do app

--- app/src/main/java/io/github/paulosalvatore/ocean_a2_02_09_2020/PerfilActivity.kt
+++ app/src/main/java/io/github/paulosalvatore/ocean_a2_02_09_2020/PerfilActivity.kt
@@ -1,10 +1,14 @@
 package io.github.paulosalvatore.ocean_a2_02_09_2020
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
+import com.jjoe64.graphview.GraphView
+import com.jjoe64.graphview.series.DataPoint
+import com.jjoe64.graphview.series.LineGraphSeries
 import kotlinx.android.synthetic.main.activity_perfil.*
 
 class PerfilActivity : AppCompatActivity() {
@@ -47,5 +51,18 @@ class PerfilActivity : AppCompatActivity() {
         builder.setMessage("Mensagem").setTitle("Titulo")
         val dialog = builder.create()
         dialog.show()
+
+        // Testando importação de uma biblioteca de gráficos
+        val graph = findViewById<View>(R.id.graph) as GraphView
+        val series: LineGraphSeries<DataPoint> = LineGraphSeries(
+            arrayOf(
+                DataPoint(0.0, 1.0),
+                DataPoint(1.0, 5.0),
+                DataPoint(2.0, 3.0),
+                DataPoint(3.0, 2.0),
+                DataPoint(4.0, 6.0)
+            )
+        )
+        graph.addSeries(series)
     }
 }
